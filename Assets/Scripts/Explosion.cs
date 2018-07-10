118a2b6cf2313bd0b69fd374cfe29b67c633d2b6
takuya_ebihara <t.ebichan0121@gmail.com>
2018-07-10 15:57:42 +0900

    左クリック時にレティクル位置にExplosionオブジェクト生成するようにしました。

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour {
+
+	public float explo_time_s;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		explo_time_s -= Time.deltaTime;
+		if(explo_time_s <= 0)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Reticle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reticle : MonoBehaviour {
+
+	public GameObject Explosion;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 mousePos = Input.mousePosition;
+		Vector3 objPos = Camera.main.ScreenToWorldPoint(mousePos);
+		objPos.z = 0;
+
+		this.transform.position = objPos;
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			Vector3 tmp = transform.position;
+			Instantiate(Explosion, new Vector3(tmp.x,tmp.y,tmp.z), Quaternion.identity);
+			Debug.Log(tmp);
+		}
+	}
+}
