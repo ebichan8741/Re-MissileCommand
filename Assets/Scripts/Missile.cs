39ba5ff2552f19b45af450f2d5ff0a113773f516
takuya_ebihara <t.ebichan0121@gmail.com>
2018-07-10 17:08:35 +0900

    暫定的に、ミサイルの動きを角度で制御できるようにしました。

---
+++ Assets/Scripts/Missile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour {
+
+	private Vector2 direction;
+	public float explo_time_s;
+	// Use this for initialization
+	void Start () {
+		direction = transform.up * -0.1f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log(transform.forward);
+		GetComponent<Rigidbody2D>().transform.Translate(direction);
+		explo_time_s -= Time.deltaTime;
+		if (explo_time_s <= 0)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/MissileGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileGenerator : MonoBehaviour {
+
+	public GameObject Missile;
+	public float MinRangeX, MaxRangeX, MinRangeY, MaxRangeY, MinRotRange, MaxRotRange;
+	public float CreateSpeed;
+
+	private float time = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time += Time.deltaTime;
+		if (time >= CreateSpeed)
+		{
+			Instantiate(Missile, new Vector3(Random.Range(MinRangeX, MaxRangeX),
+											 Random.Range(MinRangeY, MaxRangeY),
+											 0),
+										Quaternion.Euler(0,
+														 0,
+														 Random.Range(MinRotRange, MaxRotRange)));
+			time = 0;
+		}
+	}
+}
