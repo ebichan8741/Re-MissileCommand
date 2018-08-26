cce7d63502dc465fd7c257b0299545648c495ab8
takuya_ebihara <t.ebichan0121@gmail.com>
2018-08-27 01:21:50 +0900

    砲台から弾の軌跡を出す処理＆軌跡が到達してから爆発するように変更

--- Assets/Scripts/Reticle.cs
+++ Assets/Scripts/Reticle.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Reticle : MonoBehaviour {
 
 	public GameObject Explosion;
+	public GameObject ShotTrail;
+
+	private GameObject Command;
 
 	// Use this for initialization
 	void Start () {
@@ -18,10 +21,24 @@ public class Reticle : MonoBehaviour {
 
 		this.transform.position = objPos;
 
+		GameObject[] Commands = GameObject.FindGameObjectsWithTag("Command");
+		
+		float distance,work;
+		work = 10000000;
+		foreach(GameObject obj in Commands)
+		{
+			distance = Vector3.Distance(obj.transform.position, transform.position);
+			if(distance < work)
+			{
+				Command = obj;
+				work = distance;
+			}
+		}
+
 		if (Input.GetMouseButtonDown(0))
 		{
-			Vector3 tmp = transform.position;
-			Instantiate(Explosion, new Vector3(tmp.x,tmp.y,tmp.z), Quaternion.identity);
+			Vector3 tmp = Command.transform.position;
+			Instantiate(ShotTrail, new Vector3(tmp.x,tmp.y,tmp.z), Quaternion.identity);
 		}
 	}
 }
---
+++ Assets/Scripts/ShotTrail.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotTrail : MonoBehaviour {
+
+	public float shot_speed;
+	public GameObject Explosion;
+
+	private Vector3 targetDir;
+	private Vector3 targetPos;
+	private GameObject Reticle;
+
+	// Use this for initialization
+	void Start () {
+		Reticle = GameObject.FindGameObjectWithTag("Reticle");
+		targetPos = Reticle.transform.position;
+		targetDir = targetPos - transform.position;
+		targetDir = targetDir.normalized;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Rigidbody2D>().transform.Translate(targetDir * shot_speed, Space.World);
+		float distance = Vector3.Distance(targetPos, transform.position);
+		if(distance < 0.3f)
+		{
+			Destroy(gameObject);
+			Vector3 tmp = transform.position;
+			Instantiate(Explosion, new Vector3(tmp.x, tmp.y, tmp.z), Quaternion.identity);
+		}
+	}
+}
